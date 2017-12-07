636601a48e25c0a32b98fe85bc146de9da8783ea
Lukkoz <ukikoza@gmail.com>
2017-12-07 16:35:30 +0100

    ExampleFile
    Example File updated!

--- examples/SkribotApp/SkriBotApp.ino
+++ examples/SkribotApp/SkriBotApp.ino
@@ -1,21 +1,7 @@
-#include <Skriblocks.h>
-#include <Adafruit_NeoPixel.h>
-#define ROTOR_SPEED_PIN_R 6
-#define ROTOR_DIR_PIN_R 7
-#define ROTOR_SPEED_PIN_L 5
-#define ROTOR_DIR_PIN_L 4
-#define ECHO_PIN_1 10
-#define ECHO_PIN_2 12
-#define TRIG_PIN_1 9
-#define TRIG_PIN_2 11
-#define LED_DATA_PIN 2
-#define LED_DATA_PIN_1 0
-#define BT_STATE_PIN 1
-#define CLAW_PIN1 2
-#define CLAW_PIN2 8
-
+#include <SkriBlocks.h>
 #define Too_Long_Code 3
 #define   NUMPIXELS 3
+//#define DEBUG_MODE
 BlockHandler BH;
 SkriBot *robot;
 bool runCode = true;
@@ -24,11 +10,8 @@ bool transmision_recieved = false;
 void BTLOST(){
   BH.clear();
   robot->Stop();
-  robot->TurnLEDOn(255,0,0); 
-
+  robot->TurnLEDOff(); 
   runCode = false;
-  ////Serial.println("BT LOST!");
-  
 }
 
 int freeRam() 
@@ -39,31 +22,19 @@ int freeRam()
 }
 
 void setup() {
-  pinMode(BT_STATE_PIN,INPUT);
-  pinMode(A0,INPUT_PULLUP);
-  //Serial.begin(9600);
+  pinMode(EDU_BT_STATE_PIN,INPUT);
+  #if ENABLED(DEBUG_MODE)
+    Serial.begin(9600);
+  #endif
    Serial3.begin(9600);
   Serial3.setTimeout(50);
-  robot = new SkriBot();
+  robot = new SkriBot("EDU_SHIELD");
   Block::setRobot(robot);
-  robot->AddDCRotor(ROTOR_SPEED_PIN_L,ROTOR_DIR_PIN_L,"Left");          //adding rotors for movement
-  robot->AddDCRotor(ROTOR_SPEED_PIN_R,ROTOR_DIR_PIN_R,"Right");
-  robot->AddDistSensor(ECHO_PIN_1,TRIG_PIN_1,1);   //adding Distance Sensors  and naming them "Left and Right";
-  robot->AddDistSensor(ECHO_PIN_2,TRIG_PIN_2,0);
-  robot->AddLED(LED_DATA_PIN_1,1);
-  robot->AddLED(LED_DATA_PIN,0);
-  robot->AddClaw(CLAW_PIN1,CLAW_PIN2);
-  
-  
-  
+   
   robot->SetSpeed(250); 
   String messagetmp;
   char asci[30];
   char ascitmp;
-  if(digitalRead(A0) == LOW){
-    robot->MoveForward();
-    while(true){}
-  }
   robot->Stop();
 
   while(true){
@@ -71,7 +42,9 @@ void setup() {
     while(Serial3.available()){
     robot->TurnLEDOn(0,0,255);
     ascitmp = Serial3.read();
-    //Serial.print(ascitmp);
+    #if ENABLED(DEBUG_MODE)
+      Serial.print(ascitmp);
+    #endif
     BH.AllMessage[BH.messageLength] = ascitmp;
     BH.messageLength++;
       robot->TurnLEDOn(0,0,0);
@@ -93,17 +66,21 @@ void setup() {
            if(Serial3.available()){
            while(Serial3.available()){
             messagetmp = Serial3.readString();
-            //Serial.print(messagetmp);
+             #if ENABLED(DEBUG_MODE)
+              Serial.print(messagetmp);
+             #endif
             }
             if(messagetmp == "END\n"){
-              ////Serial.println("Stopping the robot!");
+              #if ENABLED(DEBUG_MODE)
+              Serial.println("Stopping the robot!");
+              #endif
               BH.clear();
               robot->Stop();
               robot->TurnLEDOn(255,255,255);
               break;
             }
             }
-              if(digitalRead(BT_STATE_PIN) == LOW)BTLOST();
+              if(digitalRead(EDU_BT_STATE_PIN) == LOW)BTLOST();
         }
         BH.clear();
         break;
@@ -113,20 +90,20 @@ void setup() {
            if(Serial3.available()){
            while(Serial3.available()){
             messagetmp = Serial3.readString();
-            ////Serial.print(messagetmp);
+             #if ENABLED(DEBUG_MODE)
+              Serial.print(messagetmp);
+             #endif
             }
             if(messagetmp == "END\n"){
-              ////Serial.println("Stopping the robot!");
               BH.clear();
               robot->Stop();
               robot->TurnLEDOn(255,255,255);
               break;
             }
             }
-              if(digitalRead(BT_STATE_PIN) == LOW)BTLOST();
+              if(digitalRead(EDU_BT_STATE_PIN) == LOW)BTLOST();
         }
         BH.clear();
-        //detachInterrupt(digitalPinToInterrupt(3));
         runCode = true;
         break;
     }
@@ -134,10 +111,8 @@ void setup() {
     if(freeRam() < 190){
       while(true){
          robot->TurnLEDOn(255,0,0); 
-         
          delay(1000);
-         robot->TurnLEDOff(); 
-         
+         robot->TurnLEDOff();        
          delay(500);
       }
     }
@@ -150,14 +125,17 @@ void setup() {
     }
     if(transmision_recieved == true){
       Serial3.println("ack");
-      ////Serial.println("Sending Cnofirmation!");
+       #if ENABLED (DEBUG_MODE)
+          Serial.println("Sending Confirmation!");
+      #endif
       transmision_recieved = false;
     }
-    //////Serial.println("Ending Block!");
-  if(digitalRead(BT_STATE_PIN) == LOW){
+    #if ENABLED(DEBUG_MODE)
+        Serial.println("Ending Block!");
+    #endif
+  if(digitalRead(EDU_BT_STATE_PIN) == LOW){
      robot->TurnLEDOn(0,0,0); 
      robot->Stop();
-     ////Serial.println("NO BT!");
   }else{
      robot->TurnLEDOn(255,255,255); 
   }
@@ -168,15 +146,6 @@ void setup() {
 }
 
 void loop() {
- /* for(int i = 0; i < 10; i++){
-    Block tmp = Block(0,0,0,0);
-    table[i] = tmp;
-    ////Serial.println(i);
-    delay(200);
-  }
-
-  */
-  // put your main code here, to run repeatedly:
   digitalWrite(Too_Long_Code,HIGH);
 }
 
