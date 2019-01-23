a070ccfb32f22fb29b8a8915afc588ed7d771e9e
Lukkoz <ukikoza@gmail.com>
2019-01-23 12:12:04 +0100

    user function bug fix
    fix pointer bug

--- examples/SkribotApp/UserFunctions.cpp
+++ examples/SkribotApp/UserFunctions.cpp
@@ -1,5 +1,9 @@
 #include "Skribot_mobile.h"
 
+                                  //put your global variables here if you want to pass some information from one instance of your function to another
+int lastRightSensor =  0;
+int lastLeftSensor  =  0;
+
 void UserFunctions_Setup(){
 	
 	//Put all setup code here - it will be run onece at the beggining of the program.
@@ -30,25 +34,25 @@ bool RIGHTSens  = Block::robot->ReadLineSensor(1);
 int UserFunction_3(int input){
 //Write your own Arduino based Function here and trigger it in app using block UserFunction_3!
 
-   int LeftSensor = robot.ReadDistSensor("D1");                  //Read distance to neares obstacle from both sensors distance. Obtained distance is in cm.
-   int RightSensor = robot.ReadDistSensor("D2");       
+   int LeftSensor = Block::robot->ReadDistSensor("D1");                  //Read distance to neares obstacle from both sensors distance. Obtained distance is in cm.
+   int RightSensor = Block::robot->ReadDistSensor("D2");       
     if(LeftSensor > RightSensor){                             //checking previous readout
         if(RightSensor > lastRightSensor){        
-          robot.FaceRight(input);                               // Turning Right  
+          Block::robot->FaceRight(input);                               // Turning Right  
         }else{
-          robot.FaceLeft(input*2);                                 // We turned Left and now we are turning Right again?! We are probably in the corner lets escape!
+          Block::robot->FaceLeft(input*2);                                 // We turned Left and now we are turning Right again?! We are probably in the corner lets escape!
         } 
     }
     if(RightSensor > LeftSensor){
       if(LeftSensor > lastLeftSensor ){
-        robot.FaceLeft(input);                                 // Turning Left
+        Block::robot->FaceLeft(input);                                 // Turning Left
       }
       else{
-        robot.FaceRight(input*2);                                 // We turned Right and now we are turning Left again?! We are probably in the corner lets escape!
+        Block::robot->FaceRight(input*2);                                 // We turned Right and now we are turning Left again?! We are probably in the corner lets escape!
       }
     }
     else{
-      robot.MoveForward();                                   //Move Forward if there are no obstacles in th way.                                 
+      Block::robot->MoveForward();                                   //Move Forward if there are no obstacles in th way.                                 
     }
     lastRightSensor = RightSensor;
     lastLeftSensor = LeftSensor;                             //Remembering last readouts
