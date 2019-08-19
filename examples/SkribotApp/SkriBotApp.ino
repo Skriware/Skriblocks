e6a6c6c373d17afb2e3ebbd1283d80b791d6ff8a
Lukkoz <ukikoza@gmail.com>
2019-08-19 16:09:09 +0200

    Update SkriBotApp.ino

--- examples/SkribotApp/SkriBotApp.ino
+++ examples/SkribotApp/SkriBotApp.ino
@@ -47,7 +47,7 @@ void setup() {
     Serial.println("DEBUG_MODE");
   #endif
   #ifdef ESP_H
-     robot = new Skribot("SKRIBRAIN");
+     robot = new Skribot("SMART_ROTOR_TEST");
   #else
     robot = new Skribot("EDU_SHIELD");
   #endif
